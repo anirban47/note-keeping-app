66164af51276043cf488b30c83966c6b868df5a7
anirban <anirban552000@gmail.com>
2021-08-14 17:02:18 +0530

    Note keeping app in react

---
+++ src/components/App.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  
+    function useStickyState(defaultValue, key) {
+        const [value, setValue] = useState(() => {
+            const stickyValue = window.localStorage.getItem(key);
+
+            return stickyValue !== null
+                ? JSON.parse(stickyValue)
+                : defaultValue;
+        });
+
+        useEffect(() => {
+            window.localStorage.setItem(key, JSON.stringify(value));
+        }, [value, setValue]);
+
+        return [value, setValue];
+    }
+
+    const [notes, updateNotes] = useStickyState([], "notes");
+
+    function addNote(noteObj) {
+        updateNotes((prevNotes) => {
+            return [...prevNotes, { key: prevNotes.length, ...noteObj }];
+        });
+    }
+
+    function deleteNote(id) {
+        updateNotes((prevNotes) => {
+            return prevNotes.filter((note, index) => {
+                return index !== id;
+            });
+        });
+    }
+
+    return (
+        <div>
+            <Header />
+            <CreateArea addClicked={addNote} />
+            {notes.map((note, index) => {
+                return (
+                    <Note
+                        key={index}
+                        id={index}
+                        title={note.title}
+                        content={note.content}
+                        deleteClicked={deleteNote}
+                    />
+                );
+            })}
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,56 @@
+import React, {useState} from "react";
+import NoteAddIcon from '@material-ui/icons/NoteAdd';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom'
+
+function CreateArea(props) {
+
+  const [isExpanded, setExpanded] = useState(false);
+  const [noteObj, updateNoteObj] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleInput(event){
+    const {name, value} = event.target;
+    updateNoteObj(prevValue => {
+      return {
+        ...prevValue,
+        [name] : value
+      }
+    });
+  }
+
+  function handleOnSubmit(event) {
+    event.preventDefault();
+    props.addClicked(noteObj);
+  }
+
+  function expand(){
+    setExpanded(true);
+  }
+
+  return (
+    <div>
+      <form className="create-note" onSubmit={handleOnSubmit}>
+        {isExpanded ? 
+          <input 
+            onChange={handleInput} 
+            name="title" 
+            placeholder="Title" 
+            value={noteObj.title}
+            /> : null}
+        <textarea 
+          onChange={handleInput} 
+          name="content" 
+          placeholder="Take a note..." 
+          value={noteObj.content} 
+          onClick={expand}
+          rows={isExpanded ? "3" : "1"} />
+        <Zoom in={isExpanded ? true : false}><Fab type="submit"><NoteAddIcon/></Fab></Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
